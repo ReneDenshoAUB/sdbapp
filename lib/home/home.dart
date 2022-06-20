8374eee34d90b41cc51adedb5d94553a63e521bb
Densho1 <den4947@gmail.com>
2022-06-20 16:30:59 +0800

    Finished HomeScreen

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -6,11 +6,53 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.pushNamed(context, '/leasescreen1'),
-          child: const Text('Get Started'),
-        ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            'assets/images/aubLogo.png',
+            width: 200,
+            fit: BoxFit.fitWidth,
+          ),
+          Image.asset(
+            'assets/images/depositIcon.png',
+            width: 190,
+            fit: BoxFit.fitWidth,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(70.0, 10.0, 70.0, 5.0),
+            child: Text(
+              'Safety Deposit Box Application',
+              style: Theme.of(context).textTheme.headline3,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(70.0, 0.0, 70.0, 0.0),
+            child: Text(
+              'Keep your valuables and documents safe and secure with our safety deposit boxes.',
+              style: Theme.of(context).textTheme.subtitle1,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+            child: SizedBox(
+              width: 230,
+              height: 55,
+              child: ElevatedButton(
+                style: Theme.of(context).elevatedButtonTheme.style,
+                onPressed: () => Navigator.pushNamed(context, '/leasescreen1'),
+                child: Text(
+                  'Get Started',
+                  style: Theme.of(context).textTheme.button,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/themes.dart
+++ lib/themes.dart
@@ -2,4 +2,38 @@ import 'package:flutter/material.dart';
 
 var appTheme = ThemeData(
   brightness: Brightness.light,
+  textTheme: const TextTheme(
+    headline3: TextStyle(
+      fontFamily: 'Gilroy-Bold',
+      fontWeight: FontWeight.w500,
+      color: Color(0xff3d3d3d),
+      fontSize: 36,
+    ),
+    subtitle1: TextStyle(
+      fontFamily: 'Gilroy',
+      color: Color(0xff868686),
+      fontWeight: FontWeight.w800,
+      fontSize: 13,
+    ),
+    button: TextStyle(
+      fontFamily: 'Gilroy-Bold',
+      fontWeight: FontWeight.w500,
+      fontSize: 28,
+      color: Colors.white,
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(28.0),
+        ),
+      ),
+      backgroundColor: MaterialStateProperty.all<Color>(
+        const Color(0xffc93033),
+      ),
+    ),
+  ),
 );
+
+var temptheme = ThemeData();
